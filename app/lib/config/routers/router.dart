24d29336c5614473e06a3f1cafa6ea4a4c705850
unknown <kevin>
2024-03-22 19:51:21 -0700

    act

--- app/lib/config/routers/router.dart
+++ app/lib/config/routers/router.dart
@@ -1,4 +1,7 @@
 import 'package:go_router/go_router.dart';
+import 'package:widgets_2024_1/presentation/screens/buttons/ButtonScreen.dart';
+import 'package:widgets_2024_1/presentation/screens/cards/CardScreen.dart';
+import 'package:widgets_2024_1/presentation/screens/home/home.dart';
 
 final appRouter = GoRouter(
   debugLogDiagnostics: true,
@@ -19,5 +22,48 @@ final appRouter = GoRouter(
       name: const CardScreen().nameScreen,
       builder: (context, state) => const CardScreen(),
     ),
+    GoRoute(
+      path: "/progress",
+      name: ProgressScreen.screenName,
+      builder: (BuildContext context, GoRouterState state) =>
+          const ProgressScreen(),
+    ),
+    GoRoute(
+      path: "/snackbars",
+      name: SnackBarScreen.screenName,
+      builder: (BuildContext context, GoRouterState state) =>
+          const SnackBarScreen(),
+    ),
+    GoRoute(
+      path: "/animated",
+      name: AnimatedScreen.screenName,
+      builder: (BuildContext context, GoRouterState state) =>
+          const AnimatedScreen(),
+    ),
+    GoRoute(
+      path: "/ui-controls",
+      name: UiControlsScreen.screenName,
+      builder: (context, state) => const UiControlsScreen(),
+    ),
+    GoRoute(
+      path: "/tutorial",
+      name: AppTutorialScreen.screenName,
+      builder: (context, state) => const AppTutorialScreen(),
+    ),
+    GoRoute(
+      path: "/infinite",
+      name: InfiniteScrollScreen.screenName,
+      builder: (context, state) => const InfiniteScrollsScreen(),
+    ),
+    GoRoute(
+      path: "/counter-river",
+      name: CounterScreen.name,
+      builder: (context, state) => const CounterScreen(),
+    ),
+    GoRoute(
+      path: "/theme-changer",
+      name: ThemeChangerScreen.name,
+      builder: (context, state) => const ThemeChangerScreen(),
+    ),
   ],
-);
+);//aa
